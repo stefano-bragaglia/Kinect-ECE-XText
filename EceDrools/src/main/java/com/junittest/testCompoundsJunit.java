0ffce0b3cbd8996f9654c6a53663fd9162e25303
cadupper <cadupper@gmail.com>
2013-11-08 15:01:01 +0100

    junit test di OR, AND, AND multiple, OR multiple

--- EceDrools/src/main/java/com/junittest/testCompoundsJunit.java
+++ EceDrools/src/main/java/com/junittest/testCompoundsJunit.java
@@ -14,7 +14,7 @@ public class testCompoundsJunit {
 
 	@SuppressWarnings("restriction")
 	@Test
-	public void testAndSimple() {
+	public void testAndSimple() {	//AND
 		ConditionInterface condContainer = new AndDescr(new MoreDescr(new NumberDescr(15),new NumberDescr(8)),new LessDescr(new NumberDescr(3),new NumberDescr(7)));
         boolean result = condContainer.validate();
         assertEquals(result, true);
@@ -35,7 +35,27 @@ public class testCompoundsJunit {
 	
 	@SuppressWarnings("restriction")
 	@Test
-	public void testOrSimple() {
+	public void testAndTriple() {	//AND AND
+		ConditionInterface condContainer = new AndDescr(new AndDescr(new LessDescr( new NumberDescr(1),new NumberDescr(3)),new LessDescr( new NumberDescr(3),new NumberDescr(7))),new LessDescr( new NumberDescr(7),new NumberDescr(9)));
+		boolean result = condContainer.validate();
+        assertEquals(result, true);
+        
+        condContainer = new AndDescr(new AndDescr(new LessDescr( new NumberDescr(1),new NumberDescr(3)),new LessDescr( new NumberDescr(11),new NumberDescr(7))),new LessDescr( new NumberDescr(7),new NumberDescr(9)));
+		result = condContainer.validate();
+        assertEquals(result, false);
+        
+        condContainer = new AndDescr(new AndDescr(new LessDescr( new NumberDescr(1),new NumberDescr(3)),new LessDescr( new NumberDescr(11),new NumberDescr(7))),new LessDescr( new NumberDescr(20),new NumberDescr(9)));
+		result = condContainer.validate();
+        assertEquals(result, false);
+        
+        condContainer = new AndDescr(new AndDescr(new LessDescr( new NumberDescr(15),new NumberDescr(3)),new LessDescr( new NumberDescr(11),new NumberDescr(7))),new LessDescr( new NumberDescr(20),new NumberDescr(9)));
+		result = condContainer.validate();
+        assertEquals(result, false);
+	}
+	
+	@SuppressWarnings("restriction")
+	@Test
+	public void testOrSimple() {	//OR
 		ConditionInterface condContainer = new OrDescr(new MoreDescr(new NumberDescr(15),new NumberDescr(8)),new LessDescr(new NumberDescr(3),new NumberDescr(7)));
         boolean result = condContainer.validate();
         assertEquals(result, true);
@@ -52,6 +72,26 @@ public class testCompoundsJunit {
         result = condContainer.validate();
         assertEquals(result, false);
        
+	}
+	
+	@SuppressWarnings("restriction")
+	@Test
+	public void testOrTriple() {	//OR OR
+		ConditionInterface condContainer = new OrDescr(new OrDescr(new LessDescr( new NumberDescr(1),new NumberDescr(3)),new LessDescr( new NumberDescr(3),new NumberDescr(7))),new LessDescr( new NumberDescr(7),new NumberDescr(9)));
+		boolean result = condContainer.validate();
+        assertEquals(result, true);
+        
+        condContainer = new OrDescr(new OrDescr(new LessDescr( new NumberDescr(10),new NumberDescr(3)),new LessDescr( new NumberDescr(3),new NumberDescr(7))),new LessDescr( new NumberDescr(7),new NumberDescr(9)));
+		result = condContainer.validate();
+        assertEquals(result, true);
+        
+        condContainer = new OrDescr(new OrDescr(new LessDescr( new NumberDescr(10),new NumberDescr(3)),new LessDescr( new NumberDescr(30),new NumberDescr(7))),new LessDescr( new NumberDescr(7),new NumberDescr(9)));
+		result = condContainer.validate();
+        assertEquals(result, true);
+        
+        condContainer = new OrDescr(new OrDescr(new LessDescr( new NumberDescr(10),new NumberDescr(3)),new LessDescr( new NumberDescr(30),new NumberDescr(7))),new LessDescr( new NumberDescr(70),new NumberDescr(9)));
+		result = condContainer.validate();
+        assertEquals(result, false);
 	}
 
 }
