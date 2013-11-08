c8d17c89a0996e72e8efd479aa60a86f3111c92e
cadupper <cadupper@gmail.com>
2013-11-08 14:38:20 +0100

    Aggiunto validate() per OR

--- EceDrools/src/main/java/com/junittest/testCompoundsJunit.java
+++ EceDrools/src/main/java/com/junittest/testCompoundsJunit.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.xtext.ecerule.model.ConditionInterface;
 import org.xtext.ecerule.model.conditions.compounds.AndDescr;
+import org.xtext.ecerule.model.conditions.compounds.OrDescr;
 import org.xtext.ecerule.model.conditions.relations.LessDescr;
 import org.xtext.ecerule.model.conditions.relations.MoreDescr;
 import org.xtext.ecerule.model.expressions.NumberDescr;
@@ -32,13 +33,25 @@ public class testCompoundsJunit {
 		
 	}
 	
-//	@SuppressWarnings("restriction")
-//	@Test
-//	public void testOrSimple() {
-//		ConditionInterface condContainer = new AndDescr(new MoreDescr(new NumberDescr(15),new NumberDescr(8)),new LessDescr(new NumberDescr(3),new NumberDescr(7)));
-//        boolean result = condContainer.validate();
-//        assertEquals(result, true);
-//		
-//	}
+	@SuppressWarnings("restriction")
+	@Test
+	public void testOrSimple() {
+		ConditionInterface condContainer = new OrDescr(new MoreDescr(new NumberDescr(15),new NumberDescr(8)),new LessDescr(new NumberDescr(3),new NumberDescr(7)));
+        boolean result = condContainer.validate();
+        assertEquals(result, true);
+
+        condContainer = new OrDescr(new MoreDescr(new NumberDescr(5),new NumberDescr(8)),new LessDescr(new NumberDescr(3),new NumberDescr(7)));
+        result = condContainer.validate();
+        assertEquals(result, true);
+        
+        condContainer = new OrDescr(new MoreDescr(new NumberDescr(15),new NumberDescr(8)),new LessDescr(new NumberDescr(10),new NumberDescr(7)));
+        result = condContainer.validate();
+        assertEquals(result, true);
+        
+        condContainer = new OrDescr(new MoreDescr(new NumberDescr(5),new NumberDescr(8)),new LessDescr(new NumberDescr(10),new NumberDescr(7)));
+        result = condContainer.validate();
+        assertEquals(result, false);
+       
+	}
 
 }
--- org.xtext.ecerule/ECE_MODEL/org/xtext/ecerule/model/conditions/compounds/OrDescr.java
+++ org.xtext.ecerule/ECE_MODEL/org/xtext/ecerule/model/conditions/compounds/OrDescr.java
@@ -12,4 +12,22 @@ import org.xtext.ecerule.model.conditions.CompoundDescr;
  */
 public class OrDescr extends CompoundDescr implements ConditionInterface {
 
+	public OrDescr(ConditionInterface cond1, ConditionInterface cond2) {
+		super(cond1, cond2);
+	}
+
+	@Override
+	public boolean validate() {
+		ConditionInterface cond1 = super.getCond1();
+		ConditionInterface cond2 = super.getCond2();
+		boolean value1= cond1.validate();
+		boolean value2 = cond2.validate();
+		if(value1 || value2){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
 }
