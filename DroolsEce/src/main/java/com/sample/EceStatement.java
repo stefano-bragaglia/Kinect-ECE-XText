1c9be330b7cef2e13af544312cb72f1b9ebfefa9
cadupper <cadupper@gmail.com>
2013-11-26 09:32:34 +0100

    Inserito progetto drools aggiornato

---
+++ DroolsEce/src/main/java/com/sample/EceStatement.java
@@ -0,0 +1,62 @@
+package com.sample;
+
+public class EceStatement {
+	private String eventPattern;
+	private String initialCondition;
+	private String finalCondition;
+	private String codeFulf;
+	private String codeViol;
+
+	public EceStatement(String eventPattern, String initialCondition,
+			String finalCondition, String codeFulf, String codeViol) {
+		super();
+		this.eventPattern = eventPattern;
+		this.initialCondition = initialCondition;
+		this.finalCondition = finalCondition;
+		this.codeFulf = codeFulf;
+		this.codeViol = codeViol;
+	}
+
+	public String getEventPattern() {
+		return eventPattern;
+	}
+
+	public void setEventPattern(String eventPattern) {
+		this.eventPattern = eventPattern;
+	}
+
+	public String getInitialCondition() {
+		return initialCondition;
+	}
+
+	public void setInitialCondition(String initialCondition) {
+		this.initialCondition = initialCondition;
+	}
+
+	public String getFinalCondition() {
+		return finalCondition;
+	}
+
+	public void setFinalCondition(String finalCondition) {
+		this.finalCondition = finalCondition;
+	}
+
+	public String getCodeFulf() {
+		return codeFulf;
+	}
+
+	public void setCodeFulf(String codeFulf) {
+		this.codeFulf = codeFulf;
+	}
+
+	public String getCodeViol() {
+		return codeViol;
+	}
+
+	public void setCodeViol(String codeViol) {
+		this.codeViol = codeViol;
+	}
+	
+	
+
+}
